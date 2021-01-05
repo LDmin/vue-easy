d29d330dc7143e0d97be12d337d76191b4d33814
LuDongmin <411726355@qq.com>
2021-01-05 09:54:37 +0800

    添加dist

---
+++ dist/useLocalStorage/index.d.ts
@@ -0,0 +1,2 @@
+declare const useLocalStorage: (key: string, initValue: any) => import("vue").Ref<string>[];
+export default useLocalStorage;
---
+++ dist/useLocalStorage/index.js
@@ -0,0 +1,17 @@
+import { ref, onMounted, watch } from 'vue';
+const useLocalStorage = (key, initValue) => {
+    const item = ref(localStorage.getItem(key));
+    if (typeof key !== 'string') {
+        console.error('第一个参数必须是string类型！');
+    }
+    watch(() => item.value, () => {
+        localStorage.setItem(key, item.value);
+    });
+    onMounted(() => {
+        if (item.value === undefined || item.value === null) {
+            item.value = initValue;
+        }
+    });
+    return [item];
+};
+export default useLocalStorage;
